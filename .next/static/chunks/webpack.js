a943fc6677610c07937716dbb1e56a03db64a6e0
Théo Sirieix <theo.sirieix@gmail.com>
2024-02-15 19:18:05 +0100

    modif login.jsx

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b04f5712ab7f9c19"; }
+/******/ 		__webpack_require__.h = function() { return "69f58995aaffcab2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -174,6 +174,21 @@
 /******/ 				if (typeof window === 'object') return window;
 /******/ 			}
 /******/ 		})();
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/harmony module decorator */
+/******/ 	!function() {
+/******/ 		__webpack_require__.hmd = function(module) {
+/******/ 			module = Object.create(module);
+/******/ 			if (!module.children) module.children = [];
+/******/ 			Object.defineProperty(module, 'exports', {
+/******/ 				enumerable: true,
+/******/ 				set: function() {
+/******/ 					throw new Error('ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ' + module.id);
+/******/ 				}
+/******/ 			});
+/******/ 			return module;
+/******/ 		};
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- components/Connexion/Login.jsx
+++ components/Connexion/Login.jsx
@@ -1,43 +1,53 @@
-import React from 'react'
-
-import styles from './Styles/Connexion.module.css'
-
+import React from 'react';
+import styles from './Styles/Connexion.module.css';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Modal, Button } from 'antd';
 
 import Signup from './Signup'
 import Signin from './Signin'
 
 export default function Login() {
 
-  const [isSignUp, setIsSignUp] = useState(true);
+  const [isSignUp, setIsSignUp] = useState(false);
   const [isSignIn, setIsSignIn] = useState(false);
 
-
-
-  let modalContent;
-	if (!isSignUp) {
-		modalContent = (
-      <Signup/>
-		);
-	} else {
-    modalContent = (
-      <Signin/>
-    )
-  }
+  const showModalUp = () => {
+    setIsSignUp(true);
+  };
+  const showModalIn = () => {
+    setIsSignIn(true);
+  };
+  
+  const handleOkUp = () => {
+    setIsSignUp(false);
+  };
+  const handleCancelUp = () => {
+    setIsSignUp(false);
+  };
+  const handleOkIn = () => {
+    setIsSignIn(false);
+  };
+  const handleCancelIn = () => {
+    setIsSignIn(false);
+  };
 
   return (
     <div className={styles.loginContainer}>
-
       <div className={styles.imagesContainer}>
         <img src='./main.gif' alt='gif' className={styles.mainImage}/>
       </div>
-
       <div className={styles.formContainer}>
         <h1>Express yourself!</h1>
-        {modalContent}
+        <Button type="primary" onClick={showModalUp}>Sign In</Button>
+        <Modal title="Basic Modal" open={isSignUp} onOk={handleOkUp} onCancel={handleCancelUp} footer={null}>
+          <Signup />
+        </Modal>
+        <Button type="primary" onClick={showModalIn}>Sign Up</Button>
+        <Modal title="Basic Modal" open={isSignIn} onOk={handleOkIn} onCancel={handleCancelIn} footer={null}>
+          <Signin />
+        </Modal>
       </div>
-
     </div>
   )
 }
